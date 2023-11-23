0cd88e9b997e0581f65e58b0e3ab5e4e744b220d
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2023-11-23 22:20:15 +0200

    fourth task

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,16 @@
+const registerForm = document.querySelector(".login-form");
+const userData = { email: "", password: "" };
 
+const onSubmit = (event) => {
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
+  if (email && password) {
+    userData.email = email;
+    userData.password = password;
+  } else {
+    alert("All form fields must be filled in");
+  }
+};
+
+registerForm.addEventListener("submit", onSubmit);
